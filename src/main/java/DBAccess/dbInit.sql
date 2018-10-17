17de20a4dfd068528782c3928e9ce589416d1dea
Simon Bojesen <simon@schonberg.dk>
2018-10-17 23:58:18 +0200

    dbInit updateret

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -1,11 +1,11 @@
- DROP DATABASE IF EXISTS LegoHouse;
+DROP DATABASE IF EXISTS LegoHouse;
 CREATE DATABASE LegoHouse;
 USE LegoHouse;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
 `id` INT NOT NULL AUTO_INCREMENT, 
-`username` VARCHAR(25) unique, 
+`email` VARCHAR(25) unique, 
 `password` VARCHAR(25),
 `role` VARCHAR(25),
 PRIMARY KEY(`id`)
@@ -14,9 +14,10 @@ PRIMARY KEY(`id`)
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
 `id` INT NOT NULL AUTO_INCREMENT,
-`lenght` INT,
+`length` INT,
 `width` INT,
 `height` INT,
+`shipped` BOOLEAN,
 `fk_userid` INT NOT NULL,
 PRIMARY KEY(`id`),
 FOREIGN KEY(`fk_userid`) REFERENCES `user`(`id`)
@@ -24,6 +25,5 @@ FOREIGN KEY(`fk_userid`) REFERENCES `user`(`id`)
 
 INSERT INTO `user`
 VALUES
-(null, 'hanne', '1234', 'customer'),
-(null, 'simon', '1234', 'employee'),
-(null, 'admin', '1234', 'admin');
+(null, 'hanne@hanne.dk', '1234', 'customer'),
+(null, 'simon@simon.dk', '1234', 'employee');
